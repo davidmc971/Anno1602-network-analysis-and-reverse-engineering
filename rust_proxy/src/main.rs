f4cea369960b55c9fcd63d9c7b0fd97821e0902d
David Alexander Pfeiffer <david.pfeiffer971@gmail.com>
2023-11-25 14:48:22 +0100

    mpsc + oneshot proof-of-concept working (no net, just thread structure)

--- rust_proxy/src/main.rs
+++ rust_proxy/src/main.rs
@@ -1,6 +1,7 @@
 #![warn(rust_2018_idioms)]
 #![allow(unused)]
 
+mod parser;
 mod tcp_proxy;
 mod udp_proxy;
 
@@ -8,12 +9,14 @@ use std::env;
 use std::error::Error;
 
 use futures::FutureExt;
+use serde::de::Expected;
 use tokio::io::copy_bidirectional;
 use tokio::net::{TcpListener, TcpStream};
+use tokio::sync::{mpsc, oneshot};
 use tokio::try_join;
 use tokio_stream::StreamExt;
 use tokio_util::codec::{BytesCodec, Decoder};
-use tracing::{debug, error, info, Level};
+use tracing::{debug, error, info, Level, span, Span};
 use tracing_subscriber::FmtSubscriber;
 
 use crate::tcp_proxy::TcpProxy;
@@ -21,17 +24,56 @@ use crate::udp_proxy::UdpProxy;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let _ = color_eyre::install();
+    {
+        // Setting up color_eyre and tracing for debug output
 
-    let mut subscriber = FmtSubscriber::builder();
-    if !cfg!(debug_assertions) {
-        subscriber = subscriber.with_max_level(Level::INFO);
-    } else {
-        subscriber = subscriber.with_max_level(Level::DEBUG);
+        let _ = color_eyre::install();
+
+        let mut subscriber = FmtSubscriber::builder();
+        if !cfg!(debug_assertions) {
+            subscriber = subscriber.with_max_level(Level::INFO);
+        } else {
+            subscriber = subscriber.with_max_level(Level::DEBUG);
+        }
+
+        tracing::subscriber::set_global_default(subscriber.finish())
+            .expect("setting default subscriber failed");
     }
 
-    tracing::subscriber::set_global_default(subscriber.finish())
-        .expect("setting default subscriber failed");
+    let (tx, mut rx) = 
+        // mpsc - multiple producer, single consumer + oneshot to send messages back to origin
+        mpsc::channel::<(String, oneshot::Sender<String>)>(32);
+
+    tokio::spawn(async move {
+        let accumulator_span = span!(Level::DEBUG, "Accumulator");
+        while let Some((message, responder)) = rx.recv().await {
+            let response = message.clone();
+
+            {
+                let entered = accumulator_span.enter();
+                debug!("Received: {}", response);
+                drop(entered);
+            }
+
+            let _ = responder.send(response);
+        }
+    });
+
+    for i in 0..2 {
+        let tx = tx.clone();
+        let thread_span = span!(Level::DEBUG, "Thread", num = i);
+        tokio::spawn(async move {
+            for j in 0..10 {
+                let (res_tx, res_rx) = oneshot::channel();
+                let _ = tx.send((format!("Message from thread {}: {}", i, j), res_tx)).await;
+                let x = res_rx.await;
+                let entered = thread_span.enter();
+                debug!("{}", x.unwrap());
+                drop(entered);
+            }
+        });
+    }
+    
 
     let addr = env::args()
         .nth(1)
