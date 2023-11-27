2475f1b6a19e51407ede99d794417a9077362514
David Alexander Pfeiffer <david.pfeiffer971@gmail.com>
2023-11-27 19:54:30 +0100

    Ze rust_proxy is working!

--- rust_proxy/src/main.rs
+++ rust_proxy/src/main.rs
@@ -7,6 +7,7 @@ mod udp_proxy;
 
 use std::env;
 use std::error::Error;
+use std::net::{IpAddr, Ipv4Addr};
 
 use futures::FutureExt;
 use serde::de::Expected;
@@ -21,6 +22,14 @@ use tracing_subscriber::FmtSubscriber;
 
 use crate::tcp_proxy::TcpProxy;
 use crate::udp_proxy::UdpProxy;
+
+#[derive(Debug, Clone)]
+pub struct Message {
+    pub data: Vec<u8>,
+    pub origin: (IpAddr, u16),
+    pub destination: (IpAddr, u16),
+    pub mask_as_address: Option<(IpAddr, u16)>
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
@@ -42,47 +51,69 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     let (tx, mut rx) = 
         // mpsc - multiple producer, single consumer + oneshot to send messages back to origin
-        mpsc::channel::<(String, oneshot::Sender<String>)>(32);
+        mpsc::channel::<(Message, oneshot::Sender<Message>)>(32);
 
     tokio::spawn(async move {
         let accumulator_span = span!(Level::DEBUG, "Accumulator");
-        while let Some((message, responder)) = rx.recv().await {
-            let response = message.clone();
+        while let Some((mut message, responder)) = rx.recv().await {
+            let mut response = message;
 
             {
                 let entered = accumulator_span.enter();
-                debug!("Received: {}", response);
+                debug!("Received: {:?}", response);
                 drop(entered);
             }
+
+            let data = response.data.as_mut_slice();
+
+            let maybe_dplay_signature = &data[20..24];
+
+            if maybe_dplay_signature == [112, 108, 97, 121] {
+                debug!("DPLAY detected, injecting proxy IP");
+                if let Some((address, port)) = response.mask_as_address {
+                    if address.is_ipv4() {
+                        if let IpAddr::V4(address) = address {
+                            data[8..12].copy_from_slice(&(address as Ipv4Addr).octets());
+                        }
+                    }
+                }
+                //    dplay_proxied_ip = self.dplay_proxied_ip
+                //    addr = list(map(int, dplay_proxied_ip.split('.')))
+                //    struct.pack_into("<BBBB", data_client, 8, addr[0], addr[1], addr[2], addr[3])
+                //    print("[DPLAY, {}] injected proxy ip".format(self.port))
+            }
+
+            debug!("Sending: {:?}", response);
 
             let _ = responder.send(response);
         }
     });
 
-    for i in 0..2 {
-        let tx = tx.clone();
-        let thread_span = span!(Level::DEBUG, "Thread", num = i);
-        tokio::spawn(async move {
-            for j in 0..10 {
-                let (res_tx, res_rx) = oneshot::channel();
-                let _ = tx.send((format!("Message from thread {}: {}", i, j), res_tx)).await;
-                let x = res_rx.await;
-                let entered = thread_span.enter();
-                debug!("{}", x.unwrap());
-                drop(entered);
-            }
-        });
-    }
+    // for i in 0..2 {
+    //     let tx = tx.clone();
+    //     let thread_span = span!(Level::DEBUG, "Thread", num = i);
+    //     tokio::spawn(async move {
+    //         for j in 0..10 {
+    //             let (res_tx, res_rx) = oneshot::channel();
+    //             let _ = tx.send((format!("Message from thread {}: {}", i, j), res_tx)).await;
+    //             let x = res_rx.await;
+    //             let entered = thread_span.enter();
+    //             debug!("{}", x.unwrap());
+    //             drop(entered);
+    //         }
+    //     });
+    // }
     
 
-    let addr = env::args()
-        .nth(1)
-        .unwrap_or_else(|| "0.0.0.0:47624".to_string());
+    let udp_proxy_session_init = UdpProxy::new("0.0.0.0".to_string(), 47624, 1024).await?;
+    let tcp_proxy_session_data = TcpProxy::new().await?;
+    let udp_proxy_game_data = UdpProxy::new("0.0.0.0".to_string(), 2350, 1024).await?;
 
-    let udp_proxy = UdpProxy::new(addr, 1024).await?;
-    let tcp_proxy = TcpProxy::new().await?;
-
-    let res = try_join!(udp_proxy.run(), tcp_proxy.run());
+    let res = try_join!(
+        udp_proxy_session_init.run(tx.clone()),
+        tcp_proxy_session_data.run(tx.clone()),
+        udp_proxy_game_data.run(tx.clone()),
+    );
 
     if res.is_err() {
         let err = res.unwrap_err();
--- rust_proxy/src/udp_proxy.rs
+++ rust_proxy/src/udp_proxy.rs
@@ -1,7 +1,15 @@
-use std::{error::Error, net::SocketAddr};
+use std::{
+    error::Error,
+    net::{IpAddr, Ipv4Addr, SocketAddr},
+};
 
-use tokio::net::UdpSocket;
+use tokio::{
+    net::UdpSocket,
+    sync::{mpsc, oneshot},
+};
 use tracing::{debug, info};
+
+use crate::Message;
 
 pub struct UdpProxy {
     socket: UdpSocket,
@@ -10,7 +18,12 @@ pub struct UdpProxy {
 }
 
 impl UdpProxy {
-    pub async fn new(address: String, buffer_size: usize) -> Result<Self, Box<dyn Error>> {
+    pub async fn new(
+        listen_address: String,
+        port: u16,
+        buffer_size: usize,
+    ) -> Result<Self, Box<dyn Error>> {
+        let address = format!("{}:{}", listen_address, port);
         let socket = UdpSocket::bind(&address).await?;
         info!("[UDP] Socket Listening on: {}", socket.local_addr()?);
 
@@ -21,27 +34,43 @@ impl UdpProxy {
         })
     }
 
-    pub async fn run(self) -> Result<(), Box<dyn Error>> {
+    pub async fn run(
+        self,
+        tx: mpsc::Sender<(Message, oneshot::Sender<Message>)>,
+    ) -> Result<(), Box<dyn Error>> {
         let Self {
             socket,
             mut buf,
             mut incoming,
         } = self;
 
+        let port = socket.local_addr().unwrap().port();
+        let host_addr = Ipv4Addr::new(10, 30, 0, 2);
+
         loop {
-            if let Some((size, peer)) = incoming {
-                let data = buf[..size].as_mut();
+            let (size, peer) = socket.recv_from(&mut buf).await?;
 
-                debug!("Incoming data:\n{:?}\n{:x?}", data, data);
+            let data = buf[..size].as_mut();
 
-                info!("Received {} bytes from {}", size, peer);
+            debug!("Incoming data: {} bytes", data.len());
 
-                let bytes_sent = socket.send_to(data, "10.30.0.2:47624").await?;
+            info!("Received {} bytes from {}", size, peer);
 
-                info!("Sent {} bytes to {}", bytes_sent, "10.30.0.2:47624");
+            let mut bytes_sent = 0;
+
+            let mut target: String;
+
+            if peer.ip().cmp(&IpAddr::V4(host_addr)).is_eq() {
+                debug!("Message from host.");
+                target = format!("10.20.0.2:{}", port);
+                bytes_sent = socket.send_to(data, &target).await?;
+            } else {
+                debug!("Message from client.");
+                target = format!("10.30.0.2:{}", port);
+                bytes_sent = socket.send_to(data, &target).await?;
             }
 
-            incoming = Some(socket.recv_from(&mut buf).await?);
+            info!("Sent {} bytes to {}", bytes_sent, target);
         }
     }
 }
